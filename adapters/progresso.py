f1aab93b5b96b6b90fd133407c28820e0959012e
Elliton-Luis <ellitonjunior05@gmail.com>
2026-09-11 14:34:16 -0300

    feat: adicione conversores de data e minutos

--- adapters/progresso.py
+++ adapters/progresso.py
@@ -1,9 +1,20 @@
 from adapters.adapter import ViagemAdapter 
+from datetime import datetime
+from zoneinfo import ZoneInfo
+
+def conversor_minutos(tempo_estimado: str) -> int:
+    horas, minutos = map(int, tempo_estimado.split(':'))
+    return horas * 60 + minutos
+
+def conversor_data_hora(data_hora: str) -> str:
+    data = datetime.strptime(data_hora, "%d/%m/%Y %H:%M")
+    data = data.replace(tzinfo=ZoneInfo("America/Bahia"))
+    return data.isoformat()
 
 class ProgressoAdapter(ViagemAdapter):
     
     def suporta(self, dados: dict) -> bool:
-     return "codigoViagem" in dados and "PRG" in dados["codigoViagem"]
+     return "codigoViagem" in dados and "cidadeOrigem" in dados and "cidadeDestino" in dados and "dataHoraSaida" in dados and "dataHoraChegada" in dados and "tempoEstimado" in dados and "valorPassagem" in dados and "assentosDisponiveis" in dados
      
     def normalizar(self,dados: dict) -> dict:
         return {
@@ -17,12 +28,13 @@ class ProgressoAdapter(ViagemAdapter):
             "cidade": dados["cidadeDestino"],
             "uf": dados["ufDestino"],
         },
-        "partida": dados["dataHoraSaida"],
-        "chegada": dados["dataHoraChegada"],
-        "duracao_minutos": 380, #Puramente para testes
+        "partida": conversor_data_hora(dados["dataHoraSaida"]),
+        "chegada": conversor_data_hora(dados["dataHoraChegada"]),
+        "duracao_minutos": conversor_minutos(dados['tempoEstimado']),
         "preco": {
-            "valor": 129.90,
+            "valor": float(dados["valorPassagem"].replace(",",".")),
             "moeda": "BRL",
         },
         "categoria": "executivo",
-        "assentos_disponiveis": 18}
+        "assentos_disponiveis": dados["assentosDisponiveis"],}
+
