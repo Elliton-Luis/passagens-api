18334b2864081dafad6ab4f5f58d4660aca1f628
Elliton-Luis <ellitonjunior05@gmail.com>
2026-09-11 10:26:21 -0300

    feat: cria o dataclass da viagem

---
+++ viagem.py
@@ -0,0 +1,14 @@
+from dataclass import dataclass
+
+@dataclass
+class Viagem:
+    id_viagem: str
+    empresa: str
+    origem: dict
+    destino: dict
+    partida: str
+    chegada: str
+    duracao_minutos: int
+    preco: dict
+    categoria: str
+    assentos_disponiveis: int
