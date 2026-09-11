38eb8e6793f8126f182c7e03be0c0cfb7b93881f
Elliton-Luis <ellitonjunior05@gmail.com>
2026-09-11 11:14:06 -0300

    feat: implementa a nova classe de adapter da progresso

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import fastapi
 
+from adapters.progresso import ProgressoAdapter
+
 app = fastapi.FastAPI()
 
 @app.get("/")
@@ -8,25 +10,5 @@ def get_example():
 
 
 @app.post("/api/v1/viagens/normalizar")
-def normalizar_progresso(viagem: dict):
-    return {
-        "id_viagem": viagem.get("codigoViagem"),
-        "empresa": "Auto Viação Progresso",
-        "origem": {
-            "cidade": viagem.get("cidadeOrigem"),
-            "uf": viagem.get("ufOrigem"),
-        },
-        "destino": {
-            "cidade": viagem.get("cidadeDestino"),
-            "uf": viagem.get("ufDestino"),
-        },
-        "partida": viagem.get("dataHoraSaida"),
-        "chegada": viagem.get("dataHoraChegada"),
-        "duracao_minutos": 380, #Puramente para testes
-        "preco": {
-            "valor": 129.90,
-            "moeda": "BRL",
-        },
-        "categoria": "executivo",
-        "assentos_disponiveis": 18,
-    }
+def normalizar_viagens(viagem: dict):
+    return ProgressoAdapter().normalizar(viagem)
